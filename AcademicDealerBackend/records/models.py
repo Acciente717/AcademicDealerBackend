2e6b51fd3547183bc7f952af6927fef248c6c527
maxbeyond <maxbeyond@pku.edu.cn>
2019-04-30 11:32:57 +0800

    delete useless file

--- AcademicDealerBackend/records/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Records(models.Model):
-    record_text = models.CharField(max_length=200)
-    def __str__(self):
-        return self.record_text
