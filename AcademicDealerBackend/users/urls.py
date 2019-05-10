bf84c25f199f65ca4207ef4c54d1354e7092680f
Acciente717 <221thunder@gmail.com>
2019-05-10 11:41:16 +0800

    Implement login functionalities. Update JSON specification.

--- AcademicDealerBackend/users/urls.py
+++ AcademicDealerBackend/users/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login')
 ]
