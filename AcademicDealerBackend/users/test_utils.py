e9078128f2d35bdc6dfae2970014f309887a0dcd
Acciente717 <221thunder@gmail.com>
2019-05-13 20:00:48 +0800

    Add automatic testing for user register.

---
+++ AcademicDealerBackend/users/test_utils.py
@@ -0,0 +1,439 @@
+__user_register_req_normal0 = {
+    "dir":"request",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"normal0@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Donald Trump",
+                    "nick_name":"tomato",
+                    "pic_url":"",
+                    "school":"Paradize",
+                    "department":"White House",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_normal1 = {
+    "dir":"request",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"normal1@test.com",
+            "password_hash":"22222"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Donald Trump",
+                    "nick_name":"potato",
+                    "pic_url":"",
+                    "school":"Paradize",
+                    "department":"White House",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_duplicate_email = {
+    "dir":"request",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"normal0@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Donald Trump",
+                    "nick_name":"banana",
+                    "pic_url":"",
+                    "school":"Paradize",
+                    "department":"White House",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_duplicate_nickname = {
+    "dir":"request",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"nickname@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Donald Trump",
+                    "nick_name":"tomato",
+                    "pic_url":"",
+                    "school":"Paradize",
+                    "department":"White House",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_invalid_date = {
+    "dir":"request",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"invalid@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Invalider",
+                    "nick_name":"invalid",
+                    "pic_url":"",
+                    "school":"Invalid",
+                    "department":"Invalid Department",
+                    "title":"Other",
+                    "enrollment_date":"01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_missing_field = {
+    "dir":"request",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"invalid@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Invalider",
+                    "pic_url":"",
+                    "school":"Invalid",
+                    "department":"Invalid Department",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_bad_req_type0 = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"invalid@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Invalider",
+                    "nick_name":"invalid",
+                    "pic_url":"",
+                    "school":"Invalid",
+                    "department":"Invalid Department",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_bad_req_type1 = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":False
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Invalider",
+                    "nick_name":"invalid",
+                    "pic_url":"",
+                    "school":"Invalid",
+                    "department":"Invalid Department",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_req_bad_req_type2 = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":False
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "real_name":"Invalider",
+                    "nick_name":"invalid",
+                    "pic_url":"",
+                    "school":"Invalid",
+                    "department":"Invalid Department",
+                    "title":"Other",
+                    "enrollment_date":"2000-01-01",
+                    "labs":{},
+                    "projects":{},
+                    "seminars":{},
+                    "comments":{},
+                    "profile":"#### Profile title\nProfile content\n"
+                }
+        }
+}
+
+__user_register_resp_normal0 = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"normal0@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":0
+                }
+        }
+}
+
+__user_register_resp_normal1 = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"normal1@test.com",
+            "password_hash":"22222"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":0
+                }
+        }
+}
+
+__user_register_resp_duplicate_email = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"normal0@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":1
+                }
+        }
+}
+
+__user_register_resp_duplicate_nickname = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":True,
+            "user_email":"nickname@test.com",
+            "password_hash":"3333"
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":1
+                }
+        }
+}
+
+__user_register_resp_invalid_date = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":False
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":2
+                }
+        }
+}
+
+__user_register_resp_missing_field = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":False
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":4
+                }
+        }
+}
+
+__user_register_resp_bad_req_type_any = {
+    "dir":"response",
+    "signature":
+        {
+            "is_user":False
+        },
+    "content_type":"account",
+    "content":
+        {
+            "action":"register",
+            "data":
+                {
+                    "status":5
+                }
+        }
+}
+
+user_register_req_normals = [
+    __user_register_req_normal0,
+    __user_register_req_normal1
+]
+
+user_register_req_duplicates = [
+    __user_register_req_duplicate_email,
+    __user_register_req_duplicate_nickname
+]
+
+user_register_req_invalids = [
+    __user_register_req_invalid_date
+]
+
+user_register_req_missing_fields = [
+    __user_register_req_missing_field
+]
+
+user_register_req_bad_req_types = [
+    __user_register_req_bad_req_type0,
+    __user_register_req_bad_req_type1,
+    __user_register_req_bad_req_type2
+]
+
+user_register_resp_normals = [
+    __user_register_resp_normal0,
+    __user_register_resp_normal1
+]
+
+user_register_resp_duplicates = [
+    __user_register_resp_duplicate_email,
+    __user_register_resp_duplicate_nickname
+]
+
+user_register_resp_invalids = [
+    __user_register_resp_invalid_date
+]
+
+user_register_resp_missing_fields = [
+    __user_register_resp_missing_field
+]
+
+user_register_resp_bad_req_types = [
+    __user_register_resp_bad_req_type_any,
+    __user_register_resp_bad_req_type_any,
+    __user_register_resp_bad_req_type_any
+]
--- AcademicDealerBackend/users/tests.py
+++ AcademicDealerBackend/users/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
+from django.urls import reverse
+from .test_utils import *
+import json
 
-# Create your tests here.
+class UsersRegisterTests(TransactionTestCase):
+
+    def test_register_normals(self):
+        '''
+        Normal register. Should be successful.
+        '''
+
+        for req, expected_resp in \
+            zip(user_register_req_normals, user_register_resp_normals):
+            resp = self.client.post(reverse('users:register'), req,
+                content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+            self.assertDictEqual(expected_resp, json.loads(resp.content))
+    
+    def test_register_duplicates(self):
+        '''
+        Duplicate register. Either duplicating email or nickname.
+        Sould return status code 1.
+        '''
+
+        for req, expected_resp in \
+            zip(user_register_req_normals, user_register_resp_normals):
+            resp = self.client.post(reverse('users:register'), req,
+                content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+            self.assertDictEqual(expected_resp, json.loads(resp.content))
+
+        for req, expected_resp in \
+            zip(user_register_req_duplicates, user_register_resp_duplicates):
+            resp = self.client.post(reverse('users:register'), req,
+                content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+            self.assertDictEqual(expected_resp, json.loads(resp.content))
+    
+    def test_register_invalids(self):
+        '''
+        Invalid field test. Date must be in the form of "yyyy-mm-dd".
+        Should return status code 2.
+        '''
+
+        for req, expected_resp in \
+            zip(user_register_req_invalids, user_register_resp_invalids):
+            resp = self.client.post(reverse('users:register'), req,
+                content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+            self.assertDictEqual(expected_resp, json.loads(resp.content))
+
+    def test_register_missing_fields(self):
+        '''
+        Missing field test.
+        Should return status code 4.
+        '''
+
+        for req, expected_resp in \
+            zip(user_register_req_missing_fields, user_register_resp_missing_fields):
+            resp = self.client.post(reverse('users:register'), req,
+                content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+            self.assertDictEqual(expected_resp, json.loads(resp.content))
+    
+    def test_register_bad_request_types(self):
+        '''
+        Missing field test.
+        Should return status code 4.
+        '''
+
+        for req, expected_resp in \
+            zip(user_register_req_bad_req_types, user_register_resp_bad_req_types):
+            resp = self.client.post(reverse('users:register'), req,
+                content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+            self.assertDictEqual(expected_resp, json.loads(resp.content))
