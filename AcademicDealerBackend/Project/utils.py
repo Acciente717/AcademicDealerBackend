57661806e15f74c2881b4a32b925989ea792dc83
Acciente717 <221thunder@gmail.com>
2019-05-16 11:41:04 +0800

    Migrating project JSON response generating.

--- AcademicDealerBackend/Project/utils.py
+++ AcademicDealerBackend/Project/utils.py
@@ -68,86 +68,51 @@ def assert_action(dic, val):
         raise BadJSONType('Invalud value in "content":"action"!')
 
 def gen_fail_response(action, status):
-    response_msg = '''
-{
+    response_msg = {
     "dir":"response",
     "content_type":"project",
     "content":
     {
-        "action":"%s",
+        "action":action,
         "data":
         {
-            "status":%d
+            "status":status
         }
     }
 }
-''' % (action, status)
-    return response_msg
+    return json.dumps(response_msg)
 
 def gen_success_response(action, status, id):
-    response_msg = '''
-{
+    response_msg = {
     "dir":"response",
     "content_type":"project",
     "content":
     {
-        "action":"%s",
+        "action":action,
         "data":
         {
-            "status":%d,
-            "id":%d
+            "status":status,
+            "id":id
         }
     }
 }
-''' % (action, status, id)
-    return response_msg
+    return json.dumps(response_msg)
 
 def gen_success_response_comment(action, status, id):
-    response_msg = '''
-{
+    response_msg = {
     "dir":"response",
     "content_type":"project",
     "content":
     {
-        "action":"%s",
+        "action":action,
         "data":
         {
-            "status":%d,
-            "comment_id":%d
+            "status":status,
+            "comment_id":id
         }
     }
 }
-''' % (action, status, id)
-    return response_msg
-
-def build_project_view_old(action, status, id, project, members, comments):
-    resp = '''\
-{
-    "dir":"response",
-    "content_type":"project",
-    "content":
-    {
-        "action":"%s",
-        "data":
-        {
-            "status":%d,
-            "id":%d,
-            "name":"%s",
-            "owner":"%s",
-            "start_date":"%s",
-            "end_date":"%s",
-            "member_total_need":%d,
-            "description":"%s",
-            "create_date":"%s",
-            "modified_date":"%s",
-            "current_members":%s,
-            "comments":%s
-        }
-    }
-}''' % (action, status, id, project.name, project.owner.email,
-        timezone.localtime(project.start_date), timezone.localtime(project.end_date), project.member_total_need,
-        project.description, timezone.localtime(project.create_date), timezone.localtime(project.modified_date), members, comments)
-    return resp
+    return json.dumps(response_msg)
 
 def build_project_view(action, status, id, project, members, comments):
     resp = {
@@ -176,59 +141,55 @@ def build_project_view(action, status, id, project, members, comments):
     return json.dumps(resp)
 
 def build_project_list_view(action, status, projects):
-    resp = '''\
-{
+    resp = {
     "dir":"response",
     "content_type":"project",
     "content":
     {
-        "action":"%s",
+        "action":action,
         "data":
         {
-            "status":%d,
-            "projects":%s,
-            "total_num":%d
+            "status":status,
+            "projects":projects[:100],
+            "total_num":len(projects)
         }
     }
-}''' % (action, status, repr(projects[:100]), len(projects))
-    return resp
+}
+    return json.dumps(resp)
 
 def build_comment_view(action, status, id, comment):
-    resp = '''\
-{
+    resp = {
     "dir":"response",
     "content_type":"project",
     "content":
     {
-        "action":"%s",
+        "action":action,
         "data":
         {
-            "status":%d,
-            "comment_id":%d,
-            "owner":"%s",
-            "create_date":"%s",
-            "modified_date":"%s",
-            "description":"%s"
+            "status":status,
+            "comment_id":id,
+            "owner":comment.owner.email,
+            "create_date":str(timezone.localtime(comment.create_date)),
+            "modified_date":str(timezone.localtime(comment.modified_date)),
+            "description":comment.description
         }
     }
-}''' % (action, status, id, comment.owner.email,
-        timezone.localtime(comment.create_date), timezone.localtime(comment.modified_date), comment.description)
-    return resp
+}
+    return json.dumps(resp)
 
 def build_search_result(action, status, ids, start, len):
-    resp = '''\
-{
+    resp = {
     "dir":"response",
     "content_type":"project",
     "content":
     {
-        "action":"%s",
+        "action":action,
         "data":
         {
-            "status":%s,
-            "ids":%s,
-            "total_num":%d
+            "status":status,
+            "ids":ids[start:start + len],
+            "total_num":len(ids)
         }
     }
-}''' % (action, status, repr(ids[start:start + len]), len(ids))
-    return resp
+}
+    return json.dumps(resp)
